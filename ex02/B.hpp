ecba38084b9805d796066b1bdb01aa6c74064b76
Xyloo <adprzyby@student.42heilbronn.de>
2024-12-15 16:24:05 +0100

    Implemented necessary functions

--- ex02/B.hpp
+++ ex02/B.hpp
@@ -6,7 +6,7 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 15:08:02 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/12/15 15:19:02 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/12/15 16:11:24 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 #include "Includes.hpp"
 
 class B : public Base {
-	
+	public:
+		
 };
--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 15:08:20 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/12/15 15:22:19 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/12/15 16:22:17 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,43 @@ Base::~Base() {}
 
 Base* generate(void) {
 	std::srand(std::time(0));
+	int random = std::rand() % 3;
+	
+	switch (random) {
+		case 0:
+			return new A();
+		case 1:
+			return new B();
+		case 2:
+			return new C();
+		default:
+			return nullptr;
+	}
 }
+
 void identify(Base* p) {
-	
+    if (dynamic_cast<A*>(p)) {
+        std::cout << "A" << std::endl;
+    } else if (dynamic_cast<B*>(p)) {
+        std::cout << "B" << std::endl;
+    } else if (dynamic_cast<C*>(p)) {
+        std::cout << "C" << std::endl;
+    } else {
+        std::cout << "Unknown type" << std::endl;
+    }
 }
+
 void identify(Base& p) {
-	
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+    } catch (std::bad_cast&) {}
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+    } catch (std::bad_cast&) {}
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+    } catch (std::bad_cast&) {}
 }
